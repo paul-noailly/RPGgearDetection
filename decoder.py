d31d4dea209684aab4ac885e4f94b7b548916319
paul <paul.noailly.pro@example.com>
2022-04-18 18:03:19 +0200

    hard fixed decoder missmatch % for , v2

--- decoder.py
+++ decoder.py
@@ -317,8 +317,8 @@ class Decoder():
             else:
                 nbs_procs = 1
             if self.completed_substats:
-                stat_amount = float(sub_dict['amount']['value'].replace('%','').replace('°',''))
-                is_percent =  '%' in sub_dict['amount']['value'] or "°" in sub_dict['amount']['value']
+                stat_amount = float(sub_dict['amount']['value'].replace('%','').replace('°','').replace(',','').replace(' ',''))
+                is_percent =  '%' in sub_dict['amount']['value'] or "°" in sub_dict['amount']['value'] or "," in sub_dict['amount']['value']
             else:
                 stat_amount = 0
                 is_percent = False
